655431ba7924161f2d4b243a60969e8ced12871a
Sergey Gernyak <sergeg1990@gmail.com>
2017-02-07 17:00:06 +0200

    Raise exception if collision has been occured
    Also provide DSL to configure the library

--- lib/awesome_importing.rb
+++ lib/awesome_importing.rb
@@ -1,5 +1,15 @@
 require "awesome_importing/version"
-require "awesome_importing/object_utils"
 
 module AwesomeImporting
+  autoload :Config, 'awesome_importing/config'
+  autoload :NameAlreadyTaken, 'awesome_importing/name_already_taken'
+  autoload :ObjectUtils, 'awesome_importing/object_utils'
+
+  # Provides config object to configure AwesomeImporting library
+  #
+  # @return [AwesomeImporting::Config]
+  def self.config(&block)
+    @_config ||= Config.new
+    block_given? ? yield(@_config) : @_config
+  end
 end
--- lib/awesome_importing/object_utils.rb
+++ lib/awesome_importing/object_utils.rb
@@ -8,12 +8,23 @@ module AwesomeImporting
     #
     # @param module_or_class [Class,Module] - Class or module which you want to importing
     # @param as [String] - A custom name for an importing module or class
+    # @raise AwesomeImporting::NameAlreadyTaken when collision has been occured and
+    #   collision checking is strict
     def importing(module_or_class, as: nil)
       mc_name = as || module_or_class.name.demodulize
-      if defined?(mc_name)
+      _importing_collision_checking(mc_name)
+      Object.const_set mc_name, module_or_class
+    end
+
+    private
+
+    def _importing_collision_checking(mc_name)
+      return unless defined?(mc_name)
+      if AwesomeImporting.config.strict_collision_checking
+        raise AwesomeImporting::NameAlreadyTaken, mc_name
+      else
         Rails.logger.warn "Trying to import `#{module_or_class.name}` as `#{mc_name}`, but there is already an object with a given name!"
       end
-      Object.const_set mc_name, module_or_class
     end
   end
 end

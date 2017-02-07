9d99f6bd2bd511e1020149c1e5cdbb0603659f78
Sergey Gernyak <sergeg1990@gmail.com>
2017-02-07 16:43:51 +0200

    Added ObjectUtils module to extend Object
    Actually this module provides single method `importing`, which is a core
    functionality of this library.

--- lib/awesome_importing.rb
+++ lib/awesome_importing.rb
@@ -1,5 +1,5 @@
 require "awesome_importing/version"
+require "awesome_importing/object_utils"
 
 module AwesomeImporting
-  # Your code goes here...
 end
---
+++ lib/awesome_importing/object_utils.rb
@@ -0,0 +1,18 @@
+module AwesomeImporting
+  module ObjectUtils
+    # Imports module or class in a current context with a given name.
+    # If a name is not spicified, the demodulized name of a module or class will be used.
+    #
+    # @param module_or_class [Class,Module] - Class or module which you want to importing
+    # @param as [String] - A custom name for an importing module or class
+    def importing(module_or_class, as: nil)
+      mc_name = as || module_or_class.name.demodulize
+      if defined?(mc_name)
+        Rails.logger.warn "Trying to import `#{module_or_class.name}` as `#{mc_name}`, but there is already an object with a given name!"
+      end
+      Object.const_set mc_name, module_or_class
+    end
+  end
+end
+
+Object.send :include, AwesomeImporting::ObjectUtils

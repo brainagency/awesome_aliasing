52b02f325cde6b9e627adcfd3c2b3466d341224a
Sergey Gernyak <sergeg1990@gmail.com>
2017-02-07 16:53:14 +0200

    Added NameAlreadyTaken exception class

---
+++ lib/awesome_importing/name_already_taken.rb
@@ -0,0 +1,13 @@
+module AwesomeImporting
+  #
+  # An exception which will be trown if collision checking is strict and collision has been occured
+  #
+  class NameAlreadyTaken < StandardError
+    # Initializes new exception instance
+    #
+    # @param const_name [String] - A name of a constant for which collision has been occured
+    def initialize(const_name)
+      super "A constant name `#{const_name}` is already taken!"
+    end
+  end
+end
